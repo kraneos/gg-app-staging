0707bb1f8f9d18eedaeaa004cf5cac55f12fd305
Agustin Polo <poloagustin@gmail.com>
2016-07-03 20:51:10 -0300

    agregado typings

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -16,8 +16,17 @@ module.exports = function(defaults) {
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
-      '@angular2-material/**/*.js',
-      'parse/**/*.js'
+      '@angular2-material/**/*.+(js|js.map)',
+      'parse/**/*.js',
+      'intl/**/*.+(js|js.map)'
+    ],
+    polyfills: [
+      'vendor/es6-shim/es6-shim.js',
+      'vendor/intl/dist/intl.js',
+      'vendor/intl/locale-data/jsonp/es-AR.js',
+      'vendor/reflect-metadata/Reflect.js',
+      'vendor/systemjs/dist/system.src.js',
+      'vendor/zone.js/dist/zone.js'
     ]
   });
 };
