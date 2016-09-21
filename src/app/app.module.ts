3d603a95ff51f42babbabaa4fbd248c113ffee14
apolo_codes <poloagustin@gmail.com>
2016-09-21 20:34:27 -0300

    agregar angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,31 @@ import { HttpModule } from '@angular/http';
 
 import { routing, appRoutingProviders } from './app.routing';
 
+import 'style!@angular2-material/core/style/core.css';
+import 'style!@angular2-material/core/overlay/overlay.css';
+
+import 'hammerjs';
+
+import {MdButtonToggleModule} from '@angular2-material/button-toggle/button-toggle';
+import {MdButtonModule} from '@angular2-material/button/button';
+import {MdCheckboxModule} from '@angular2-material/checkbox/checkbox';
+import {MdRadioModule} from '@angular2-material/radio/radio';
+import {MdSlideToggleModule} from '@angular2-material/slide-toggle/slide-toggle';
+import {MdSliderModule} from '@angular2-material/slider/slider';
+import {MdSidenavModule} from '@angular2-material/sidenav/sidenav';
+import {MdListModule} from '@angular2-material/list/list';
+import {MdGridListModule} from '@angular2-material/grid-list/grid-list';
+import {MdCardModule} from '@angular2-material/card/card';
+import {MdIconModule} from '@angular2-material/icon/icon';
+import {MdProgressCircleModule} from '@angular2-material/progress-circle/progress-circle';
+import {MdProgressBarModule} from '@angular2-material/progress-bar/progress-bar';
+import {MdInputModule} from '@angular2-material/input/input';
+import {MdTabsModule} from '@angular2-material/tabs/tabs';
+import {MdToolbarModule} from '@angular2-material/toolbar/toolbar';
+import {MdTooltipModule} from '@angular2-material/tooltip/tooltip';
+import {MdRippleModule} from '@angular2-material/core/ripple/ripple';
+import {MdMenuModule} from '@angular2-material/menu/menu';
+
 import { ParseService } from './shared/services/parse.service';
 import { LoginService } from './shared/services/login.service';
 import { CurrentUserService } from './shared/services/current-user.service';
@@ -54,7 +79,26 @@ import { RegisterComponent } from './register/register.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    MdButtonModule.forRoot(),
+    MdButtonToggleModule.forRoot(),
+    MdCardModule.forRoot(),
+    MdCheckboxModule.forRoot(),
+    MdGridListModule.forRoot(),
+    MdIconModule.forRoot(),
+    MdInputModule.forRoot(),
+    MdListModule.forRoot(),
+    MdMenuModule.forRoot(),
+    MdProgressBarModule.forRoot(),
+    MdProgressCircleModule.forRoot(),
+    MdRadioModule.forRoot(),
+    MdRippleModule.forRoot(),
+    MdSidenavModule.forRoot(),
+    MdSliderModule.forRoot(),
+    MdSlideToggleModule.forRoot(),
+    MdTabsModule.forRoot(),
+    MdToolbarModule.forRoot(),
+    MdTooltipModule.forRoot()
   ],
   providers: [
     appRoutingProviders,
