26a2a3458aec9c020c4aef657252e38a40189320
Agustin Polo <poloagustin@gmail.com>
2016-09-28 00:10:14 -0300

    ...

--- src/app/client-policies/client-policies.component.spec.ts
+++ src/app/client-policies/client-policies.component.spec.ts
@@ -2,9 +2,12 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { ClientPoliciesComponent } from './client-policies.component';
+import { PoliciesService } from '../shared/services/policies.service';
+import { ParseService } from '../shared/services/parse.service';
 
 describe('Component: ClientPolicies', () => {
   it('should create an instance', () => {
+    let policiesService = new PoliciesService()
     let component = new ClientPoliciesComponent();
     expect(component).toBeTruthy();
   });
